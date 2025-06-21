dbe01012c06289ae79f5be4042207198bd452564
Mritunjay Saini <140881890+Mritunjay-Saini@users.noreply.github.com>
2025-06-21 08:55:09 +0530

    Create data.js

---
+++ src/components/data.js
@@ -0,0 +1,46 @@
+const items = [
+  {
+    id: 1,
+    title: "Pancakes",
+    category: "Breakfast",
+    price: 5.99,
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    title: "Burger",
+    category: "Lunch",
+    price: 8.99,
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 3,
+    title: "Milkshake",
+    category: "Shakes",
+    price: 4.99,
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 4,
+    title: "Egg Sandwich",
+    category: "Breakfast",
+    price: 6.49,
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 5,
+    title: "Steak",
+    category: "Lunch",
+    price: 12.99,
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 6,
+    title: "Strawberry Shake",
+    category: "Shakes",
+    price: 5.49,
+    img: "https://via.placeholder.com/150",
+  },
+];
+
+export default items;
